7f70242905eeb2398851920626a3f4d9ef9dbc96
KSN <ksn@ksn>
2025-07-12 17:00:37 -0300

    fix: the env setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const app = require('./app');
 const { connectPostgres } = require('./utils/database/pg');
 const { connectMongo } = require('./utils/database/mongo');
+
 const PORT = process.env.PORT;
 
 async function init(){
